8776ef5918906e4ef213c5875f43721ce531e308
forlink <49800833+forlink@users.noreply.github.com>
2019-07-12 22:47:51 +0900

    Add files via upload

--- csv_toolkit/csv2groovy.py
+++ csv_toolkit/csv2groovy.py
@@ -1,28 +1,20 @@
 import csv
 import os  
+import glob
 
-def file_list(file_dir):
-    L=[]
-    for root, dirs, files in os.walk(file_dir):  
-        for file in files:  
-            if os.path.splitext(file)[1] == '.csv':  
-                L.append(os.path.join(root, file))
-    return L  
-
-def get_dict(file_list, key):
+def get_dict():
     new_dict = {}
-    for filenm in file_list:
+    for filenm in glob.glob('./csv/*.csv'):
         with open(filenm, 'rt') as csv_file:
             next(csv_file)        
             data = csv.DictReader(csv_file, delimiter=",")
             for row in data:
-                linestr = 'fields c' + row[key] + '_' + row['label'] + ' pos(' + row['pbyte'] + ',' +  row['pbit'] + '),' + row['bitlen']
-                new_dict.setdefault(row[key], []).append(linestr)
+                linestr = 'fields c' + row['canid'] + '_' + row['label'] + ' pos(' + row['pbyte'] + ',' +  row['pbit'] + '),' + row['bitlen']
+                new_dict.setdefault(row['canid'], []).append(linestr)
     return new_dict
 
-
 #main:
-dict = get_dict(file_list('./csv/'), 'canid')
+dict = get_dict()
 for k, v in dict.items():
     with open('./groovy/' + k + '.groovy','w') as output:
         output.writelines('package model.jp\n\n')
@@ -33,3 +25,4 @@ for k, v in dict.items():
         output.writelines('\n'.join(v))
         output.writelines('\n\n}')
 
+print(os.listdir('.'))
